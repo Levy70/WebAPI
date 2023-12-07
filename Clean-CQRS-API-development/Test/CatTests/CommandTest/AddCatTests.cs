e0cb284b1712edba6a3f28d7e9dfd86df3af096e
Levy70 <levis-mbaku.akanji@student.nbi-handelsakademin.se>
2023-12-07 03:12:21 +0100

    Added command tests

---
+++ Clean-CQRS-API-development/Test/CatTests/CommandTest/AddCatTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.Commands.Cats;
+using Application.Commands.Cats.AddCat;
+using Application.Dtos;
+using Infrastructure.Database;
+
+namespace Test.CatTests.CommandTest
+{
+    [TestFixture]
+    public class AddCatTests
+    {
+        private MockDatabase _mockDatabase;
+        private AddCatCommandHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialisera mockdatabasen och hanteraren innan varje test 
+            _mockDatabase = new MockDatabase();
+            _handler = new AddCatCommandHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ValidAddCat_ReturnsTrue()
+        {
+            // Arrange
+            var command = new AddCatCommand(new CatDto { Name = "New Cat" });
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            var newcatinDB = _mockDatabase.Cats.FirstOrDefault(cat => cat.Name == "New Cat");
+
+            Assert.IsNotNull(newcatinDB);
+            Assert.That(newcatinDB.Name, Is.EqualTo("New Cat"));
+        }
+
+        [Test]
+        public async Task Handle_invalidAddCat_ReturnsTrue()
+        {
+            // Arrange
+            var command = new AddCatCommand(new CatDto { Name = "" });
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+    }
+}
